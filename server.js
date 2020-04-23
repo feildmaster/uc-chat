8a04fa45a4569d06d6d0c270abf8bebba8d17740
Glitch (hello-express) <none>
2020-04-23 22:08:17 +0000

    ✊🚗 Checkpoint ./package.json:1714294/27 ./server.js:1714294/281

--- server.js
+++ server.js
@@ -10,13 +10,3 @@ const listener = app.listen(process.env.PORT, () => {
 });
 
 //real stuff
-const puppeteer = require('puppeteer');
- 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
- 
-  await browser.close();
-})();
