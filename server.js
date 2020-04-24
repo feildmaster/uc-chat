57f8ac958e9d0c570de17f81c08579efecdb7787
Glitch (hello-express) <none>
2020-04-24 23:55:23 +0000

    ⛄️🍚 Checkpoint ./server.js:1714294/1329 ./watch.json:1714294/2

--- server.js
+++ server.js
@@ -10,3 +10,39 @@ const listener = app.listen(process.env.PORT, () => {
 });
 
 //real stuff
+const https = require('https');
+
+//boilerplate https post request, better to have fine control than a library
+//do NOT put https:// part of url, it expects everything after that
+function reqHttps(url, callback, body) {
+  const hostname = url.split('/')[0];
+  const options = {
+    hostname: hostname,
+    port: 443,
+    path: encodeURI(url.slice(url.indexOf('/'))),
+    method: 'POST',
+    headers: {
+      Accept: "*/*",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-US,en;q=0.9,es;q=0.8",
+      Connection: "keep-alive",
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      'Content-Length': body ? body.length : 0,
+      Cookie: process.env.COOKIE,
+      Host: hostname,
+      Origin: 'https://' + hostname,
+      Referer: 'https://' + hostname,
+      "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"
+    }
+  };
+  const req = https.request(options, (res) => {
+    //console.log('statusCode:', res.statusCode);
+    //console.log('headers:', res.headers);
+    let total = '';
+    res.on('data', (d) => {total += d});
+    res.on('end', () => {callback ? callback(total) : null});
+  });
+  req.on('error', console.error.bind(console));
+  if (body) req.write(body);
+  req.end();
+}
