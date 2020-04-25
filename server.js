cf2995d039d30b563ccce1022cea3aecba1743c1
Glitch (hello-express) <none>
2020-04-25 00:07:23 +0000

    💘🌜 Checkpoint ./server.js:1714294/4220 ./watch.json:1714294/1

--- server.js
+++ server.js
@@ -3,46 +3,55 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 app.get("/", (request, response) => {
-  response.send('oof');
+  response.send("oof");
 });
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
 //real stuff
-const https = require('https');
+const https = require("https");
+
+reqHttps("undercards.net/SignIn", process.env.LOGINBODY, "", r =>
+  console.log(r)
+);
 
 //boilerplate https post request, better to have fine control than a library
 //do NOT put https:// part of url, it expects everything after that
-function reqHttps(url, callback, body) {
-  const hostname = url.split('/')[0];
+function reqHttps(url, body, cookie, callback) {
+  const hostname = url.split("/")[0];
   const options = {
     hostname: hostname,
     port: 443,
-    path: encodeURI(url.slice(url.indexOf('/'))),
-    method: 'POST',
+    path: encodeURI(url.slice(url.indexOf("/"))),
+    method: "POST",
     headers: {
       Accept: "*/*",
       "Accept-Encoding": "gzip, deflate, br",
       "Accept-Language": "en-US,en;q=0.9,es;q=0.8",
       Connection: "keep-alive",
       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-      'Content-Length': body ? body.length : 0,
-      Cookie: process.env.COOKIE,
+      "Content-Length": body ? body.length : 0,
+      Cookie: cookie,
       Host: hostname,
-      Origin: 'https://' + hostname,
-      Referer: 'https://' + hostname,
-      "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"
+      Origin: "https://" + hostname,
+      Referer: "https://" + hostname,
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"
     }
   };
-  const req = https.request(options, (res) => {
-    //console.log('statusCode:', res.statusCode);
-    //console.log('headers:', res.headers);
-    let total = '';
-    res.on('data', (d) => {total += d});
-    res.on('end', () => {callback ? callback(total) : null});
+  const req = https.request(options, res => {
+    console.log("statusCode:", res.statusCode);
+    console.log("headers:", res.headers);
+    let total = "";
+    res.on("data", d => {
+      total += d;
+    });
+    res.on("end", () => {
+      callback ? callback(total) : null;
+    });
   });
-  req.on('error', console.error.bind(console));
+  req.on("error", console.error.bind(console));
   if (body) req.write(body);
   req.end();
 }
