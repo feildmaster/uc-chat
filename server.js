f4f822fab6bb934c05d41dcedabccc7132423602
Glitch (hello-express) <none>
2020-04-25 03:42:29 +0000

    🌹🌉 Checkpoint ./server.js:1714294/1076

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const listener = app.listen(process.env.PORT, () => {
 const https = require("https");
 const agent = https.globalAgent;
 const WebSocket = require("ws");
-const ws = new WebSocket("wss://undercards.net/chat");
 
 //ws stuff
 ws.on("open", function open() {
@@ -46,6 +45,30 @@ reqHttps("undercards.net/SignIn", process.env.LOGINBODY, headers => {
   let setCookie = headers["set-cookie"];
   let auth = setCookie.map(cookie => cookie.split(";")[0]).join("; ") + ";";
   console.log(auth);
+  
+  //ws stuff
+  const hostname = "undercards.net/chat";
+  const options = {
+    hostname: hostname,
+    port: 443,
+    path: encodeURI(url.slice(url.indexOf("/"))),
+    method: "POST",
+    headers: {
+      Accept: "*/*",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-US,en;q=0.9,es;q=0.8",
+      Connection: "keep-alive",
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "Content-Length": 0,
+      //Cookie: cookie,
+      Host: hostname,
+      Origin: "https://" + hostname,
+      Referer: "https://" + hostname,
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"
+    }
+  };
+  const ws = new WebSocket("wss://undercards.net/chat", [], options);
 });
 
 //boilerplate https post request, better to have fine control than a library
