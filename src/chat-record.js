ba87ed5370db65bacf563dbe2a518607eceaf007
Glitch (uc-chat) <none>
2020-04-29 01:30:29 +0000

    🍯💺 Checkpoint ./src/chat-record.js:15401775/302

--- src/chat-record.js
+++ src/chat-record.js
@@ -1,9 +1,18 @@
 const record = new Map();
+const limit = 200;
 
 exports.add = ({id, user}, room) => {
   const {id: userid, username} = user;
   record.set(id, {
     room, userid, username,
   });
+  
+  if (record.size > limit) {
+    const itr = record.keys();
+    do { // In the off chance theres more than 1 stray
+      const key = itr.next();
+      record.delete(key);
+    } while (record.size > limit);
+  }
 };
 exports.get = (id) => record.get(id);
