20a12c3cad6150b1d471c155b525b250273e6086
Glitch (uc-chat) <none>
2020-04-29 09:18:29 +0000

    🐞🕵 Checkpoint ./src/getMessage.js:15401775/5

--- src/getMessage.js
+++ src/getMessage.js
@@ -5,7 +5,7 @@ const entities = new Entities();
 const specialCharacters = /([`|*~]|^>)/g;
 const emoteRegex = /:[^\s:]+(?:\\_[^\s_:]+)+:/g;
 
-function getMessage({ user, message, me }) {
+function getMessage({ user, message = '', me }) {
   let safeMessage = entities.decode(parseMessageEmotes(message.replace(/_/g, '\\_'))) // Scrub underscores
     .replace(emoteRegex, (match) => match.replace(/\\/g, '')) // Allow default emoji, because it's cute
     .replace(specialCharacters, '\\$1'); // Scrub discord characters
