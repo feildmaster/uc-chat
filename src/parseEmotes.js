2a4c456fa8da373e73a3adabf782f216e4278e85
feildmaster <feildmaster@users.noreply.github.com>
2020-05-24 18:17:33 -0500

    chore: limit emoji counters

--- src/parseEmotes.js
+++ src/parseEmotes.js
@@ -8,17 +8,26 @@ const missing = stats.counters('emojiMissing');
 const missingGif = stats.counters('emojiMissingAnimated');
 
 function parseMessageEmotes(message = '') {
+  const previous = new Set(); // Limit one of each emoji per message
+
   //images are displayed to the web browser as <img src="images/emotes/Disturbed_Burger_Pants.png" />
   const parsedMessage = message.replace(emoteRegex, (match, $1, $2) => {
     const emote = $1.replace(/\\/g, '');
     const discord = emoji[emote];
     
-    popular.get(emote).increment();
+    const incremented = previous.has(emote);
+
+    if (!incremented) {
+      popular.get(emote).increment();
+      previous.add(emote);
+    }
 
     if (discord) return discord;
 
-    const counter = $2 === 'gif' ? missingGif : missing;
-    counter.get(emote).increment();
+    if (!incremented) {
+      const counter = $2 === 'gif' ? missingGif : missing;
+      counter.get(emote).increment();
+    }
 
     return `:${emote}:`;
   });
