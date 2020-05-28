eb64c9dfac80bca5ec36ff2fca671d6a9d835a1a
feildmaster <feildmaster@users.noreply.github.com>
2020-05-27 22:22:32 -0500

    fix: reconnect when *not* exiting

--- src/bot.js
+++ src/bot.js
@@ -187,7 +187,7 @@ undercards.on('connect', () => { // Join rooms
   });
   console.debug('Socket Closed');
   // We can technically try and reconnect here
-  if (process.exitCode !== undefined) {
+  if (process.exitCode === undefined) {
     discord.editStatus('idle');
     reconnectUC(5000);
   }
