8ce0e26fb3f9a5582c517d8a0040c0b05159c25e
feildmaster <feildmaster@users.noreply.github.com>
2020-06-24 19:55:45 -0500

    chore: remove missing counters for new emotes

--- src/discordEmoji.js
+++ src/discordEmoji.js
@@ -4,12 +4,18 @@ const stats = require('./stats');
 
 const emoji = {};
 const popular = stats.counters('emoji');
+const missing = stats.counters('emojiMissing');
+const missingGif = stats.counters('emojiMissingAnimated');
 
 function add({ key, id, name, animated }) {
   if (!id) return;
   emoji[key] = `<${animated?'a':''}:${name || key.substring(0, key.indexOf('.'))}:${id}>`;
   emoji[id] = true;
   popular.get(key); // Add entry to popularity list
+  const list = animated ? missingGif : missing;
+  if (list.has(key)) {
+    list.remove(key);
+  }
 }
 
 function hook(snapshot) {
